5b6756a1b46ef7074733c0e810bfa0da0f6feecd
Fernando Gonzalez <fgonzalez@promaticmedia.cl>
2026-02-08 20:37:53 -0300

    ajustes js problema 3

--- assets/js/problema3.js
+++ assets/js/problema3.js
@@ -1,12 +1,12 @@
 function validacion() {
-    let number1 = Number(document.querySelector('#selector-1').value);
-    let number2 = Number(document.querySelector('#selector-2').value);
-    let number3 = Number(document.querySelector('#selector-3').value);
+    let number1 = document.querySelector('#selector-1').value;
+    let number2 = document.querySelector('#selector-2').value;
+    let number3 = document.querySelector('#selector-3').value;
 
-    let passInput = "" + number1 + number2 + number3;
+    let passInput = number1 + number2 + number3;
     
-    let pass1 = "911";
-    let pass2 = "714";
+    let pass1 = 911;
+    let pass2 = 714;
     
     if (passInput == pass1) {
         let message = document.querySelector('#pass-message');
